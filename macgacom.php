4deb3d7159533600805084982de495f6fd34b909
macga <51775505+macga@users.noreply.github.com>
2019-07-04 11:42:13 +0900

    Create macgacom.php

---
+++ macgacom.php
@@ -0,0 +1,19 @@
+<?
+$LastModified = gmdate("D d M Y H:i:s", filemtime($HTTP_SERVER_VARS[SCRIPT_FILENAME])); 
+header("Last-Modified: $LastModified GMT"); 
+header("ETag: \"$LastModified\"");
+?> 
+<html>
+<head>
+<title>맥가닷컴 [WWW.MACGA.COM 3ND]</title>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+</head>
+<frameset cols="2%,98%" border="0">
+        <frame src="/macgacom/bgsound.php">
+        <frame src="/macgacom/m3nd_01.php"> 
+        </noframes>
+</frameset><noframes></noframes>
+</html>
+<!-- 소스보기는 자제해 주세요. -->
+<!-- 최근수정일: 2007.10.12 -->
+<!-- Copyright www.macga.com All rights reserved 2005-2007 -->
